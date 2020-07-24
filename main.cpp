224903625aa1f3e63d28bfdb3add8bd049f280e1
syddfyuan <syddfyuan@tencent.com>
2020-07-24 21:18:29 +0800

    Adjust env.

--- main.cpp
+++ main.cpp
@@ -4,12 +4,8 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
-#include <iostream>
-
-using Vec3 = glm::vec3;
-using Point = glm::vec3;
+#include "Source/Rasterize.h"
+#include "Source/StaticLighting.h"
 
 #define EPSILON 0.0000001f
 
@@ -65,28 +61,37 @@ private:
 };
 
 
-int main() {
+int main() 
+{
     glfwInit();
-    
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-    
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-    
     std::cout << extensionCount << " extensions supported\n";
     
     glm::mat4 matrix;
     glm::vec4 vec;
     auto test = matrix * vec;
-    
-    while(!glfwWindowShouldClose(window)) {
+
+	int UpsampleFactor = 4;
+	int HighResolutionSignalSizeX = 256 * 4;
+	int HighResolutionSignalSizeY = 256 * 4;
+
+	DistanceFieldRasterPolicy RasterPolicy(UpsampleFactor, HighResolutionSignalSizeX, HighResolutionSignalSizeY);
+	TriangleRasterizer<DistanceFieldRasterPolicy> DistanceFieldRasterizer(RasterPolicy);
+	StaticLightingVertex v0;
+	StaticLightingVertex v1;
+	StaticLightingVertex v2;
+	Vec2 uv0;
+	Vec2 uv1;
+	Vec2 uv2;
+	DistanceFieldRasterizer.DrawTriangle(v0, v1, v2, uv0, uv1, uv2);
+    while(!glfwWindowShouldClose(window)) 
+	{
         glfwPollEvents();
-    }
-    
+    } 
     glfwDestroyWindow(window);
-    
-    glfwTerminate();
-    
+    glfwTerminate();    
     return 0;
 }
